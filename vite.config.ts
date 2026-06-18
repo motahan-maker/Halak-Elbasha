1747da7b0223d6d1542f4ff3eba405037e6418dd
motahan-maker <2.04278836e+08+motahan-maker@users.noreply.github.com>
2026-06-18 12:11:03 +0000

    fix: build error and update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import tsConfigPaths from "vite-tsconfig-paths";
-import { tanstackStart } from "@tanstack/react-start/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 
 export default defineConfig({
   plugins: [
+    tanstackStart(),
     tailwindcss(),
     react(),
-    tanstackStart(),
-    tsConfigPaths(),
   ],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
